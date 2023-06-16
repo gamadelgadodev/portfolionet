7860252467d7b70f3d3805004beb20f242d0d797
gamadelgado <mdelgado@tecnolika.com>
2023-06-15 22:05:17 -0600

    firstcommit

---
+++ portfolioGamaliel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace portfolioGamaliel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
